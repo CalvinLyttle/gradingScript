559f77b22e75d757b31981bc32748e006382232a
CalvinLyttle <CalLyttle@gmail.com>
2021-10-21 22:46:13 -0400

    Made script adaptable to any assignment name

--- testRunner.bat
+++ testRunner.bat
@@ -1,4 +1,6 @@
 @echo off
-move C:\Users\CalLy\Downloads\lab7.py C:\Users\CalLy\Documents\School\2021F\TA\Lab7
-"C:\Users\CalLy\AppData\Local\Programs\Python\Python37-32\python.exe" "C:\Users\CalLy\Documents\School\2021F\TA\Lab7\lab7_tests.py"
+set assignmentName=lab7
+echo %test_value%
+move C:\Users\CalLy\Downloads\%assignmentName%.py C:\Users\CalLy\Documents\School\2021F\TA\%assignmentName%
+"C:\Users\CalLy\AppData\Local\Programs\Python\Python37-32\python.exe" "C:\Users\CalLy\Documents\School\2021F\TA\%assignmentName%\%assignmentName%_test.py"
 pause

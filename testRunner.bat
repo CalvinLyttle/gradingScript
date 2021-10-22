c195b27e8ea1b11994768d71f4233f6174b52b45
CalvinLyttle <CalLyttle@gmail.com>
2021-10-21 22:00:07 -0400

    Add script

---
+++ testRunner.bat
@@ -0,0 +1,4 @@
+@echo off
+move C:\Users\CalLy\Downloads\lab7.py C:\Users\CalLy\Documents\School\2021F\TA\Lab7
+"C:\Users\CalLy\AppData\Local\Programs\Python\Python37-32\python.exe" "C:\Users\CalLy\Documents\School\2021F\TA\Lab7\lab7_tests.py"
+pause

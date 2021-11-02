03273c09ef89fb812e6c6cc51b732e193747a40e
CalvinLyttle <CalLyttle@gmail.com>
2021-11-01 23:04:38 -0400

    updated local instillation of python

--- testRunner.bat
+++ testRunner.bat
@@ -1,6 +1,6 @@
 @echo off
-set assignmentName=lab7
+set assignmentName=hw5
 echo %test_value%
 move C:\Users\CalLy\Downloads\%assignmentName%.py C:\Users\CalLy\Documents\School\2021F\TA\%assignmentName%
-"C:\Users\CalLy\AppData\Local\Programs\Python\Python37-32\python.exe" "C:\Users\CalLy\Documents\School\2021F\TA\%assignmentName%\%assignmentName%_test.py"
+"C:\Users\CalLy\AppData\Local\Programs\Python\Python310\python.exe" "C:\Users\CalLy\Documents\School\2021F\TA\%assignmentName%\%assignmentName%_test.py"
 pause
